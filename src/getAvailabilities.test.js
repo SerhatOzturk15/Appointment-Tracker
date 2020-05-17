8709baa8c768dd336785ffd6a3b66c04a2aa8929
SerhatOzturk15 <serhatozt15@gmail.com>
2020-05-17 17:54:20 +0200

    improved tests for recurring openings

--- src/getAvailabilities.test.js
+++ src/getAvailabilities.test.js
@@ -199,4 +199,149 @@ describe("getAvailabilities", () => {
       );
     });
   });
+
+  describe("no appointments booked in that day", () => {
+    beforeEach(async () => {
+      await knex("events").insert([
+        {
+          kind: "appointment",
+          starts_at: new Date("2014-09-11 10:30"),
+          ends_at: new Date("2014-09-11 11:30")
+        },
+        {
+          kind: "opening",
+          starts_at: new Date("2014-08-04 09:30"),
+          ends_at: new Date("2014-08-04 13:00"),
+          weekly_recurring: true
+        }
+      ]);
+    });
+
+    it("test 1", async () => {
+      const availabilities = await getAvailabilities(new Date("2014-08-10"));
+      expect(availabilities.length).toBe(7);
+
+      expect(String(availabilities[0].date)).toBe(
+        String(new Date("2014-08-10"))
+      );
+      expect(availabilities[0].slots).toEqual([]);
+
+      expect(String(availabilities[1].date)).toBe(
+        String(new Date("2014-08-11"))
+      );
+      expect(availabilities[1].slots).toEqual([
+        "9:30",
+        "10:00",
+        "10:30",
+        "11:00",
+        "11:30",
+        "12:00",
+        "12:30"
+      ]);
+    });
+  });
+
+  describe("when numberofDays is more than 7", () => {
+    beforeEach(async () => {
+      await knex("events").insert([
+        {
+          kind: "appointment",
+          starts_at: new Date("2014-08-18 10:30"),
+          ends_at: new Date("2014-08-18 11:30")
+        },
+        {
+          kind: "appointment",
+          starts_at: new Date("2014-08-25 10:30"),
+          ends_at: new Date("2014-08-25 11:30")
+        },
+        {
+          kind: "appointment",
+          starts_at: new Date("2014-08-11 10:30"),
+          ends_at: new Date("2014-08-11 11:30")
+        },
+        {
+          kind: "opening",
+          starts_at: new Date("2014-08-04 09:30"),
+          ends_at: new Date("2014-08-04 12:30"),
+          weekly_recurring: true
+        }
+      ]);
+    });
+
+    it("checks weekly occurings", async () => {
+      const availabilities = await getAvailabilities(new Date("2014-08-08"), 40);
+      expect(availabilities.length).toBe(40);
+
+      expect(String(availabilities[2].date)).toBe(
+        String(new Date("2014-08-10"))
+      );
+      expect(availabilities[0].slots).toEqual([]);
+
+      expect(String(availabilities[3].date)).toBe(
+        String(new Date("2014-08-11"))
+      );
+      expect(availabilities[3].slots).toEqual([
+        "9:30",
+        "10:00",
+        "11:30",
+        "12:00",
+      ]);
+      expect(String(availabilities[10].date)).toBe(
+        String(new Date("2014-08-18"))
+      );
+      expect(availabilities[10].slots).toEqual([
+        "9:30",
+        "10:00",
+        "11:30",
+        "12:00",
+      ]);
+       expect(String(availabilities[17].date)).toBe(
+         String(new Date("2014-08-25"))
+       );
+       expect(availabilities[17].slots).toEqual([
+         "9:30",
+         "10:00",
+         "11:30",
+         "12:00",
+       ]);
+    });
+  });
+  describe("when numberofDays is more than 7", () => {
+    beforeEach(async () => {
+      await knex("events").insert([
+        {
+          kind: "appointment",
+          starts_at: new Date("2014-08-11 10:30"),
+          ends_at: new Date("2014-08-11 11:30")
+        },
+        {
+          kind: "opening",
+          starts_at: new Date("2014-08-11 09:30"),
+          ends_at: new Date("2014-08-11 12:30"),
+          weekly_recurring: false
+        }
+      ]);
+    });
+
+    it("if there is no weekly occurings", async () => {
+      const availabilities = await getAvailabilities(new Date("2014-08-11"), 25);
+      expect(availabilities.length).toBe(25);
+
+      expect(String(availabilities[0].date)).toBe(
+        String(new Date("2014-08-11"))
+      );
+      expect(availabilities[0].slots).toEqual([
+        "9:30",
+        "10:00",
+        "11:30",
+        "12:00",
+      ]);
+
+      expect(String(availabilities[7].date)).toBe(
+        String(new Date("2014-08-18"))
+      );
+      expect(availabilities[7].slots).toEqual([]);
+
+    });
+  });
 });
